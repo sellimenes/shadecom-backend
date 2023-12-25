b67b4ea9444e71bee0b03315b2794d579defab1e
Selim Enes <sellim81@gmail.com>
2023-12-25 20:46:23 +0300

    env error test

--- initializers/loadEnvVariables.go
+++ initializers/loadEnvVariables.go
@@ -2,15 +2,22 @@ package initializers
 
 import (
 	"log"
+	"os"
+	"path/filepath"
 
 	"github.com/joho/godotenv"
 )
 
 func LoadEnvVariables() {
-	err := godotenv.Load()
+	ex, err := os.Executable()
+	if err != nil {
+		panic(err)
+	}
+	envPath := filepath.Join(filepath.Dir(ex), ".env")	
+
+	err = godotenv.Load(envPath)
 
 	if err != nil {
 		log.Fatal("Error loading .env file")
-		log.Fatal(err)
 	}
 }
