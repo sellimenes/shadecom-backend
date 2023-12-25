ae5b5e0c803495f8e3c6974351a3601d1f281422
Selim Enes <sellim81@gmail.com>
2023-12-25 20:52:25 +0300

    railway config test

--- initializers/loadEnvVariables.go
+++ initializers/loadEnvVariables.go
@@ -15,9 +15,10 @@ func LoadEnvVariables() {
 	}
 	envPath := filepath.Join(filepath.Dir(ex), ".env")	
 
-	err = godotenv.Load(envPath)
-
-	if err != nil {
-		log.Fatal("Error loading .env file")
+	if _, exists := os.LookupEnv("RAILWAY_ENVIRONMENT"); !exists {
+		err = godotenv.Load(envPath)
+		if err != nil {
+			log.Fatal("Error loading .env file")
+		}
 	}
 }
