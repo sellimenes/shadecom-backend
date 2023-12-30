06c2b45f75ea069604ca20c3eb548dd8561faf45
Selim Enes <sellim81@gmail.com>
2023-12-30 16:27:38 +0300

    loadenv fix on railway && local

--- initializers/loadEnvVariables.go
+++ initializers/loadEnvVariables.go
@@ -3,23 +3,31 @@ package initializers
 import (
 	"log"
 	"os"
-	"path/filepath"
 
 	"github.com/joho/godotenv"
 )
 
-func LoadEnvVariables() {
-	ex, err := os.Executable()
-	if err != nil {
-		panic(err)
-	}
-	envPath := filepath.Join(filepath.Dir(ex), ".env")	
-	// fmt.Println(envPath)
+// func LoadEnvVariables() {
+// 	ex, err := os.Executable()
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	envPath := filepath.Join(filepath.Dir(ex), ".env")
+// 	// fmt.Println(envPath)
 
-	if _, exists := os.LookupEnv("RAILWAY_ENVIRONMENT"); !exists {
-		err = godotenv.Load(envPath)
-		if err != nil {
-			log.Fatal("Error loading .env file")
-		}
-	}
+// 	if _, exists := os.LookupEnv("RAILWAY_ENVIRONMENT"); !exists {
+// 		err = godotenv.Load(envPath)
+// 		if err != nil {
+// 			log.Fatal("Error loading .env file")
+// 		}
+// 	}
+// }
+
+func LoadEnvVariables() {
+    if _, exists := os.LookupEnv("RAILWAY_ENVIRONMENT"); !exists {
+        err := godotenv.Load()
+        if err != nil {
+            log.Fatal("Error loading .env file")
+        }
+    }
 }
